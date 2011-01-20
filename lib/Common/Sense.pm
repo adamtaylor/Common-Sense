731bf1e4c6d4b230c3f509efea1804824c90be80
Adam Taylor <adamjctaylor@gmail.com>
2011-01-20 00:33:19 +0000

    Add a bit more dzil magic

--- lib/Common/Sense.pm
+++ lib/Common/Sense.pm
@@ -3,9 +3,25 @@ package Common::Sense;
 
 use Modern::Perl;
 
-
 sub import {
     Modern::Perl->import();
 }
+
+=head1 SYNOPSIS
+
+    use Common::Sense
+
+=head1 DESCRIPTION
+
+See L<Modern::Perl>.
+
+A redditor suggested 'use Common::Sense' and I thought it was more amusing than
+use Modern::Perl.
+
+http://www.reddit.com/r/perl/comments/f4e14/please_share_your_modern_perl_boilerplate/c1d7q0r
+
+There's no point actually using this... :)
+
+=cut
 
 1;
