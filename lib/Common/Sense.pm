a595bf3fee6df7aeef11ad16c4b793fbf068806b
Adam Taylor <adamjctaylor@gmail.com>
2011-01-20 00:22:53 +0000

    Screw it, just import Modern::Perl

--- lib/Common/Sense.pm
+++ lib/Common/Sense.pm
@@ -1,18 +1,11 @@
 # ABSTRACT: Like Modern::Perl but more amusing to use
 package Common::Sense;
 
-use 5.010_000;
+use Modern::Perl;
 
-use strict;
-use warnings;
-
-use autodie;
 
 sub import {
-    warnings->import();
-    strict->import();
-    feature->import( ':5.10' );
-    autodie->import();
+    Modern::Perl->import();
 }
 
 1;
